7896ead60ef2808e471196a9e55990cd451ccb86
AlickShabanovich <alikshabanovich@gmail.com>
2019-11-11 18:52:12 +0300

    lesson 71 setState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -14,7 +15,7 @@ const ProfileInfo = (props) => {
             </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                ava + description
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
         </div>
     )
