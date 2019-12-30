11bd2971772673ad58f3b8036127a5f8f705f031
AlickShabanovich <alikshabanovich@gmail.com>
2019-12-30 18:55:52 +0300

    lessons 98 captcha

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,13 +7,16 @@ import {login} from "../../redux/auth-reducer";
 import {Redirect} from "react-router-dom";
 import style from "./../common/FormsControls/FormsControls.module.css";
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 {createField("Email", "email", [required], Input)}
                 {createField("Password", "password", [required], Input, {type: "password"})}
                 {createField(null, "rememberMe", [], Input, {type: "checkbox"}, "remember me")}
+                { captchaUrl && <img src={captchaUrl} />}
+                { captchaUrl &&  createField("Symbols from image", "captcha", [required], Input, {}) }
+
                 {error && <div className={style.formSummaryError}>
                     {error}
                 </div>}
@@ -29,7 +32,7 @@ const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe);
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
 
     if (props.isAuth) {
@@ -37,10 +40,11 @@ const Login = (props) => {
     }
     return <div>
         <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit}/>
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
     </div>
 }
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 export default connect(mapStateToProps, {login})(Login);
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,7 +9,7 @@ import {Textarea} from "../../common/FormsControls/FormsControls";
 const MyPosts = React.memo(props => {
     let postsElements = [...props.posts]
         .reverse()
-        .map(p => <Post message={p.post} likesCount={p.likesCount}/>);
+        .map(p => <Post key={p.id} message={p.post} likesCount={p.likesCount}/>);
 
     let newPostElement = React.createRef();
 
